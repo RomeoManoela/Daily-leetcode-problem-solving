ab72c0455e4bbe2c34770d062b68d5029abca3d5
romeomanoela <romeomanoela18@gmail.com>
2025-02-09 23:08:48 +0300

    adding remove_duplicate function

--- easy.py
+++ easy.py
@@ -79,6 +79,17 @@ def merge_two_sorted_list(list1: ListNode, list2: ListNode) -> ListNode  :
 
     return dummy.next
 
+# remove duplicate from sorted list
+def remove_duplicates(self, nums: list[int]) -> int:
+    if not nums:
+        return 0
+    i: int = 0
+    for j in range(1, len(nums)):
+        if nums[j] != nums[i]:
+            i += 1
+            nums[i] = nums[j]
+    return i + 1
+
 
 
 
