eb13370a271e1ebf6f50061c90dd175ca1423756
romeomanoela <romeomanoela18@gmail.com>
2025-02-19 22:04:31 +0300

    adding length_of_last_word and plus_one functions

--- easy.py
+++ easy.py
@@ -8,13 +8,14 @@ def is_palindrome(nbr: int) -> bool:
         return False
     if len(lst) == 1:
         return True
-    for i in range(len(lst)//2):
+    for i in range(len(lst) // 2):
         first = lst[i]
         last = lst[-i - 1]
         print(first, last)
         if first != last:
             return False
     return True
+
 
 # longest comon prefix in an array of string
 def longest_common_prefix(strs: list[str]) -> str:
@@ -28,12 +29,14 @@ def longest_common_prefix(strs: list[str]) -> str:
             return common_prefix
     return common_prefix
 
+
 def has_all(chrs: str, i: int, strs: list) -> bool:
     has: bool = True
     for st in strs:
         if st[i] != chrs:
             has = False
     return has
+
 
 # valid parentheses
 def is_valid(s: str) -> bool:
@@ -42,25 +45,28 @@ def is_valid(s: str) -> bool:
     dq: deque = deque()
     for char in s:
         print(dq, char)
-        if char in ['(', '{', '[' ]:
+        if char in ["(", "{", "["]:
             dq.append(char)
-        elif char == ')':
-            if len(dq) == 0 or dq.pop() != '(':
+        elif char == ")":
+            if len(dq) == 0 or dq.pop() != "(":
                 return False
-        elif char == ']':
-            if len(dq) == 0 or dq.pop() != '[':
+        elif char == "]":
+            if len(dq) == 0 or dq.pop() != "[":
                 return False
-        elif char == '}':
-            if len(dq) == 0 or dq.pop() != '{':
+        elif char == "}":
+            if len(dq) == 0 or dq.pop() != "{":
                 return False
     return len(dq) == 0
+
 
 class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
+
+
 # Merge two sorted list
-def merge_two_sorted_list(list1: ListNode, list2: ListNode) -> ListNode  :
+def merge_two_sorted_list(list1: ListNode, list2: ListNode) -> ListNode:
     dummy = ListNode()
     current = dummy
     while list1 and list2:
@@ -79,6 +85,7 @@ def merge_two_sorted_list(list1: ListNode, list2: ListNode) -> ListNode  :
 
     return dummy.next
 
+
 # remove duplicate from sorted list
 def remove_duplicates(self, nums: list[int]) -> int:
     if not nums:
@@ -89,8 +96,10 @@ def remove_duplicates(self, nums: list[int]) -> int:
             i += 1
             nums[i] = nums[j]
     return i + 1
+
+
 # remove element in an array
-def remove_element( nums: list[int], val: int) -> int:
+def remove_element(nums: list[int], val: int) -> int:
     result: list[int] = []
     i: int = 0
     for num in nums:
@@ -100,15 +109,17 @@ def remove_element( nums: list[int], val: int) -> int:
     nums[:] = result
     return i
 
+
 # index of the first occurrence of str2 in str1
 def str_str(haystack: str, needle: str) -> int:
     if not needle in haystack:
         return -1
 
     for i in range(len(haystack) - len(needle) + 1):
-        print(haystack[i:i+len(needle)], i)
-        if haystack[i:i + len(needle)] == needle:
+        print(haystack[i : i + len(needle)], i)
+        if haystack[i : i + len(needle)] == needle:
             return i
+
 
 # search insert position
 def search_insert(nums: list[int], target: int) -> int:
@@ -126,8 +137,29 @@ def search_insert(nums: list[int], target: int) -> int:
     return left
 
 
+# last word length
+def length_of_last_word(s: str) -> int:
+    strs: list[str] = s.split(" ")
+    print(strs)
+    last_word: str = [item for item in strs if item != ""].pop()
+    return len(last_word)
+
+
+# PLus one
+def plus_one(digits: list[int]) -> list[int]:
+    n: int = len(digits) - 1
+    for i in range(n, -1, -1):
+        if digits[i] < 9:
+            digits[i] += 1
+            return digits
+        digits[i] = 0
+    return [1] + digits
+
+
 def main():
-    print(search_insert([1,3,5,6],2 ))
+    print(length_of_last_word("romeo  manoela     zadd  "))
+    print(plus_one([9]))
+
 
 if __name__ == "__main__":
     main()
