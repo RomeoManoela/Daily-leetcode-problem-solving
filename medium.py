eb1a8552eb5508e57c2fc8926cfdf5be35854402
romeomanoela <romeomanoela18@gmail.com>
2025-04-19 15:29:42 +0300

    adding linked list merge sort

--- medium.py
+++ medium.py
@@ -1,3 +1,8 @@
+from typing import Optional
+
+from easy import ListNode
+
+
 def generate_parenthesis(n: int) -> list[str]:
     def backtrack(s, left, right):
         if len(s) == 2 * n:
@@ -11,6 +16,41 @@ def generate_parenthesis(n: int) -> list[str]:
     result = []
     backtrack("", 0, 0)
     return result
+
+def sort_list(self, head: Optional[ListNode]) -> Optional[ListNode]:
+
+    if not head or not head.next:
+        return head
+
+    slow, fast = head, head.next
+    while fast and fast.next:
+        slow = slow.next
+        fast = fast.next.next
+
+    mid = slow.next
+    slow.next = None
+
+    left = self.sortList(head)
+    right = self.sortList(mid)
+
+    return merge_ll(left, right)
+
+
+def merge_ll(l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+    dummy = ListNode(0)
+    tail = dummy
+
+    while l1 and l2:
+        if l1.val < l2.val:
+            tail.next = l1
+            l1 = l1.next
+        else:
+            tail.next = l2
+            l2 = l2.next
+        tail = tail.next
+
+    tail.next = l1 if l1 else l2
+    return dummy.next
 
 
 def main():
